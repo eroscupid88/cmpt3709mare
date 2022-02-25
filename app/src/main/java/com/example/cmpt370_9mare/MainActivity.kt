8d8b67bed8f521c7bf668312c4b4add3cbbef43c
Eroscupid <vutuanduong2003@yahoo.com>
2022-02-24 22:29:42 -0600

    add change to file

--- app/src/main/java/com/example/cmpt370_9mare/MainActivity.kt
+++ app/src/main/java/com/example/cmpt370_9mare/MainActivity.kt
@@ -16,83 +16,13 @@ import java.time.format.DateTimeFormatter
 
 private const val TAG = "MainActivity"
 
-@RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
-    private var calendarRecycleView: RecyclerView? = null
-    private var monthYearText: TextView? = null
-    private var selectedDate: LocalDate? = null
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initWidgets()
-        selectedDate = LocalDate.now()
-        logging()
-        setMonthView()
+        Log.i(TAG,"INFO: MainActivity Active!")
     }
-
-    private fun logging(){
-        Log.i(TAG, "INFO: calendar app up and running")
-        Log.i(TAG, "INFO: current local date is : $selectedDate")
-    }
-
-    private fun monthYearFromDate(date: LocalDate?): String? {
-        val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MMMM yyyy")
-        return date?.format(formatter)
-    }
-
-    private fun initWidgets() {
-        calendarRecycleView = findViewById(R.id.calendarRecycleView)
-        monthYearText = findViewById(R.id.month_year)
-    }
-
-
-    private fun setMonthView() {
-        monthYearText!!.setText(monthYearFromDate(selectedDate).toString())
-        val daysInMonth: ArrayList<String> = daysInMonthArray(selectedDate!!)
-        for (x:String in daysInMonth){
-            Log.d(TAG,"$x")
-        }
-        calendarRecycleView!!.adapter = CalendarAdapter(daysInMonth)
-        calendarRecycleView!!.layoutManager= GridLayoutManager(applicationContext,7)
-
-        Log.d(TAG,"DEBUG: calling MainActivity/setMonthView()")
-    }
-
-    private fun daysInMonthArray(date:LocalDate): ArrayList<String> {
-        // maximum 31 days in month
-        var daysInMonthArray: ArrayList<String> = ArrayList(35)
-        var yearMonth: YearMonth = YearMonth.from(date)
-        var daysInMonth: Int = yearMonth.lengthOfMonth()
-        var firstOfMonth : LocalDate = selectedDate!!.withDayOfMonth(1)
-        var dayOfWeek : Int = firstOfMonth.dayOfWeek.value
-        for (x :Int in 1..42) {
-            Log.d(TAG, "DEBUG: checking week $x")
-            when{
-                 x <= dayOfWeek || x > daysInMonth+dayOfWeek-> daysInMonthArray.add("")
-                else -> daysInMonthArray.add((x - dayOfWeek).toString())
-            }
-        }
-        Log.d(TAG,"DEBUG: $yearMonth, $daysInMonth,$firstOfMonth,$dayOfWeek,$selectedDate")
-
-        return daysInMonthArray
-    }
-
-    fun nextMonthAction(view: View) {
-        selectedDate = selectedDate!!.plusMonths(1)
-        setMonthView()
-    }
-    fun previousMonthAction(view: View) {
-        selectedDate = selectedDate!!.minusMonths(1)
-        setMonthView()
-    }
-    fun eventShowAction(view: View){
-        Log.i(TAG,"eventShowClick!!")
-        TODO("Need to implement click to show event ")
-    }
-    // add something to check
 
 }
 
--- app/src/main/java/com/example/cmpt370_9mare/adapter/CalendarAdapter.kt
+++ app/src/main/java/com/example/cmpt370_9mare/adapter/CalendarAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.cmpt370_9mare.adapter
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cmpt370_9mare.R
 
@@ -17,20 +21,48 @@ class CalendarAdapter(
 
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
-        val view : View?= LayoutInflater.from(parent.context)
+        val view : View?= LayoutInflater
+            .from(parent.context)
             .inflate(R.layout.calendar_cell,parent,false)
         val layoutPrams: ViewGroup.LayoutParams? = view!!.layoutParams
         layoutPrams!!.height = (parent.height * 0.1).toInt()
+        view.accessibilityDelegate = Accessibility
         return CalendarViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
         holder.dayOfMonth.text = daysOfMonth[position]
+        holder.dayOfMonth.setOnClickListener {
+//            val action = LetterListFragmentDirections.actionLetterListFragmentToWordListFragment(letter= holder.button.text.toString())
+//            holder.view.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount(): Int {
-
         return daysOfMonth.size
+    }
+
+    // Setup custom accessibility delegate to set the text read with
+    // an accessibility service
+    companion object Accessibility : View.AccessibilityDelegate() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun onInitializeAccessibilityNodeInfo(
+            host: View?,
+            info: AccessibilityNodeInfo?
+        ) {
+            super.onInitializeAccessibilityNodeInfo(host, info)
+            // With `null` as the second argument to [AccessibilityAction], the
+            // accessibility service announces "double tap to activate".
+            // If a custom string is provided,
+            // it announces "double tap to <custom string>".
+            val customString = host?.context?.getString(R.string.hello)
+            val customClick =
+                AccessibilityNodeInfo.AccessibilityAction(
+                    AccessibilityNodeInfo.ACTION_CLICK,
+                    customString
+                )
+            info?.addAction(customClick)
+        }
     }
 
 
