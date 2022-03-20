e2e0c12386cfd95a551e68dfec92670601ddb377
nln157 <nln157@usask.ca>
2022-03-19 20:18:57 -0600

    Expandable list on home fragment

---
+++ app/src/main/java/com/example/cmpt370_9mare/ui/home/HomeExpandableAdapter.kt
@@ -0,0 +1,102 @@
+package com.example.cmpt370_9mare.ui.home
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.cmpt370_9mare.databinding.EventItemBinding
+import com.example.cmpt370_9mare.databinding.EventViewBinding
+import com.example.cmpt370_9mare.databinding.GroupEventBinding
+
+class HomeExpandableAdapter  internal constructor(
+    private val context: Context,
+    private val titleList: List<String>,
+    private val dataList: HashMap<String, List<String>>
+) : BaseExpandableListAdapter() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private lateinit var groupBinding: GroupEventBinding
+    private lateinit var itemBinding: EventItemBinding
+
+
+    override fun getGroupCount(): Int {
+        return this.titleList.size
+    }
+
+    override fun getChildrenCount(listPosition: Int): Int {
+        return this.dataList[this.titleList[listPosition]]!!.size
+    }
+
+    override fun getGroup(listPosition: Int): Any {
+        return this.titleList[listPosition]
+    }
+
+    override fun getChild(listPosition: Int, expandableListPosition: Int): Any {
+        return this.dataList[this.titleList[listPosition]]!![expandableListPosition]
+    }
+
+    override fun getGroupId(listPosition: Int): Long {
+        return listPosition.toLong()
+    }
+
+    override fun getChildId(listPosition: Int, expandableListPosition: Int): Long {
+        return expandableListPosition.toLong()
+    }
+
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun getGroupView(listPosition: Int, isExpanded: Boolean, view: View?, parent: ViewGroup?): View {
+        var convertView = view
+        val holder: GroupViewHolder
+        if (convertView == null) {
+            groupBinding = GroupEventBinding.inflate(inflater,parent,false)
+            convertView = groupBinding.root
+            holder = GroupViewHolder()
+            holder.label = groupBinding.listEvent
+            convertView.tag = holder
+        } else {
+            holder = convertView.tag as GroupViewHolder
+        }
+        val listTitle = getGroup(listPosition) as String
+        holder.label!!.text = listTitle
+        return convertView
+
+    }
+
+    override fun getChildView(listPosition: Int, expandableListPosition: Int, isLastChild: Boolean, view: View?, parent: ViewGroup?): View {
+        var convertView = view
+        val holder: ItemViewHolder
+        if (convertView == null){
+            itemBinding = EventItemBinding.inflate(inflater,parent,false)
+            convertView = itemBinding.root
+            holder = ItemViewHolder()
+            holder.label = itemBinding.expandedListItem
+            convertView.tag = holder
+        }else{
+            holder = convertView.tag as ItemViewHolder
+        }
+
+        val expandableListText = getChild(listPosition, expandableListPosition) as String
+        holder.label!!.text = expandableListText
+        return convertView
+    }
+
+    override fun isChildSelectable(p0: Int, p1: Int): Boolean {
+        return true
+    }
+
+    inner class ItemViewHolder {
+        internal var label: TextView? = null
+    }
+
+    inner class GroupViewHolder {
+        internal var label: TextView? = null
+    }
+
+
+}
--- app/src/main/java/com/example/cmpt370_9mare/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cmpt370_9mare/ui/home/HomeFragment.kt
@@ -1,23 +1,14 @@
 package com.example.cmpt370_9mare.ui.home
 
-import android.app.AlertDialog
-import android.text.InputType
-import android.view.*
-import android.widget.EditText
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ExpandableListAdapter
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cmpt370_9mare.ScheduleApplication
 import com.example.cmpt370_9mare.ScheduleEventViewModel
@@ -25,7 +16,6 @@ import com.example.cmpt370_9mare.ScheduleEventViewModelFactory
 import com.example.cmpt370_9mare.data.schedule_event.ScheduleEvent
 
 import com.example.cmpt370_9mare.databinding.FragmentHomeBinding
-import com.example.cmpt370_9mare.ui.dashboard.HomeAdapter
 
 @RequiresApi(Build.VERSION_CODES.O)
 class HomeFragment : Fragment() {
@@ -43,6 +33,24 @@ class HomeFragment : Fragment() {
     }
 
     private lateinit var recyclerView: RecyclerView
+    private var adapter: ExpandableListAdapter? = null
+    private var titleList: List<String>? = null
+
+    val data: HashMap<String, List<String>> get() {
+        val listData = HashMap<String, List<String>>()
+        val appleMobiles = ArrayList<String>()
+        appleMobiles.add(viewModel.today)
+        appleMobiles.add("iPhone 8 Plus")
+        appleMobiles.add("iPhone X")
+        appleMobiles.add("iPhone 7 Plus")
+        appleMobiles.add("iPhone 7")
+        appleMobiles.add("iPhone 6 Plus")
+        listData["Apple"] = appleMobiles
+        listData["Apple2"] = appleMobiles
+        listData["Apple3"] = appleMobiles
+        return listData
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -50,7 +58,9 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -60,26 +70,41 @@ class HomeFragment : Fragment() {
             viewModel = homeViewModel
             homeFragment = this@HomeFragment
         }
+//
+//        recyclerView = binding.homeListRecyclerView
+//
+//        val homeAdapter = HomeAdapter { }
+//
+//        recyclerView.adapter = homeAdapter
+//        // Attach an observer on the allItems list to update the UI automatically when the data changes.
+//        viewModel.allEvents.observe(this.viewLifecycleOwner) { items ->
+//            items.let {
+//                val list: MutableList<ScheduleEvent> = mutableListOf<ScheduleEvent>()
+//                for (event in it) {
+//                    if (event.date == homeViewModel.getToday()) {
+//                        list.add(event)
+//                    }
+//                }
+//                homeAdapter.submitList(list)
+//            }
+//        }
+        val expandableListView = binding.expandableListView
+        val listData = data
+        titleList = ArrayList(listData.keys)
+        adapter = HomeExpandableAdapter(this.requireContext().applicationContext, titleList as ArrayList<String>, listData)
+        expandableListView.setAdapter(adapter)
+        //setupExpandableListView();
 
-        recyclerView = binding.homeListRecyclerView
-
-        val homeAdapter = HomeAdapter { }
-
-        recyclerView.adapter = homeAdapter
-        // Attach an observer on the allItems list to update the UI automatically when the data changes.
-        viewModel.allEvents.observe(this.viewLifecycleOwner) { items ->
-            items.let {
-                val list: MutableList<ScheduleEvent> = mutableListOf<ScheduleEvent>()
-                for (event in it) {
-                    if (event.date == homeViewModel.getToday()) {
-                        list.add(event)
-                    }
-                }
-                homeAdapter.submitList(list)
-            }
-        }
 
     }
+
+//    private fun setupExpandableListView() {
+//        val expandableListView = binding.expandableListView
+//        val listData = data
+//        titleList = ArrayList(listData.keys)
+//        adapter = HomeExpandableAdapter(requireContext(), titleList as ArrayList<String>, listData)
+//        expandableListView.setAdapter(adapter)
+//    }
 
 
     fun goToNextDay() {
