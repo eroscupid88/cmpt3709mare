d39d652b2ae4a9abf118b73f7384cb40cd132b63
Kyle Holtby <krh951@usask.ca>
2022-03-28 21:34:39 -0600

    Added TC4 to ModifyTest

--- app/src/androidTest/java/ca/nomosnow/cmpt370_9mare/ModifyTest.kt
+++ app/src/androidTest/java/ca/nomosnow/cmpt370_9mare/ModifyTest.kt
@@ -13,12 +13,17 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class ModifyTest : BaseTest() {
+    /*
+        This test ensures that all data when attempting to modify an event
+        is binded to the inputs as its initial value.
+     */
     @Test
     fun TC1_text_copies_to_update_fragment() {
         val date = "${(2023..9999).random()}-04-02"
         val testTxt = "Testing"
         val testTitle = "TC1 First event"
 
+        // Create event with all txt inputs with values
         onView(withId(R.id.navigation_calendar)).perform(click())
         onView(withId(R.id.floatingActionButton)).perform(click())
         onView(withId(R.id.input_title)).perform(
@@ -45,6 +50,8 @@ class ModifyTest : BaseTest() {
         onView(withId(R.id.layout_create_event_fragment)).perform(swipeDown())
         onView(withId(R.id.submit_create_event)).perform(click())
 
+        // Attempt to modify and ensure that all data was pushed to modify
+        // event parameters
         onView(withId(R.id.navigation_dashboard)).perform(click())
         onView(withId(R.id.event_list_recycler_view)).perform(ScrollToBottom())
         onView(withText(testTitle)).perform(click())
@@ -57,10 +64,16 @@ class ModifyTest : BaseTest() {
         onView(withId(R.id.event_url)).perform(scrollTo()).check(matches(withText("$testTxt URL")))
         onView(withId(R.id.event_notes)).perform(scrollTo())
             .check(matches(withText("$testTxt Notes")))
+
+        // Delete event
         onView(withId(R.id.delete_event)).perform(scrollTo(), click())
         onView(withText("Confirm")).perform(click())
     }
 
+    /*
+        This test ensures that modifying a parameter changes the
+        resulting event.
+     */
     @Test
     fun TC2_modifying_text_updates_event() {
         val oldDate = "${(2023..9999).random()}-04-02"
@@ -68,7 +81,10 @@ class ModifyTest : BaseTest() {
         val testTitle = "TC2 First event"
         val testNewTitle = "TC2 First event modified"
 
+        // Create event
         createEvent(testTitle, oldDate)
+
+        // Modify the title and date of existing event
         onView(withId(R.id.navigation_dashboard)).perform(click())
         onView(withId(R.id.event_list_recycler_view)).perform(ScrollToBottom())
         onView(withText(testTitle)).perform(click())
@@ -80,26 +96,69 @@ class ModifyTest : BaseTest() {
         )
         onView(withId(R.id.inputDate)).perform(SetButtonText(newDate))
         onView(withId(R.id.submit_create_event)).perform(click())
+
+        // Check to see if event was infact modified
         onView(withId(R.id.event_list_recycler_view)).perform(ScrollToBottom())
         onView(withText(testNewTitle)).perform(click())
         onView(withText(newDate)).check(matches(isDisplayed()))
         onView(withText("Edit")).perform(click())
+
+        // Delete event
         onView(withId(R.id.delete_event)).perform(scrollTo(), click())
         onView(withText("Confirm")).perform(click())
     }
 
+    /*
+        This test ensures that if it modifies itself it will not
+        cause conflicts.
+     */
     @Test
     fun TC3_no_conflicts_on_modifying_own_event() {
         val testTitle = "TC3 First event"
 
+        // Create and enter event
         createEvent(testTitle)
         onView(withId(R.id.navigation_dashboard)).perform(click())
         onView(withId(R.id.event_list_recycler_view)).perform(ScrollToBottom())
         onView(withText(testTitle)).perform(click())
         onView(withText("Edit")).perform(click())
+
+        // Modify with conflict to and attempt to delete
         onView(withId(R.id.conflict_check)).perform(click())
         onView(withId(R.id.submit_create_event)).perform(click())
         onView(withId(R.id.search_event)).check(matches(isDisplayed()))
         deleteEvent(testTitle)
+    }
+
+    /*
+        This test ensures that a user can modify an event so that it is
+        all-day.
+     */
+    @Test
+    fun TC4_change_time_to_allday() {
+        val testTitle = "TC4 First event"
+
+        // Create and enter event
+        createEvent(testTitle)
+        onView(withId(R.id.navigation_dashboard)).perform(click())
+        onView(withId(R.id.event_list_recycler_view)).perform(ScrollToBottom())
+
+        // Modify event with all-day selected
+        onView(withText(testTitle)).perform(click())
+        onView(withText("Edit")).perform(click())
+        onView(withId(R.id.all_day)).perform(scrollTo(), click())
+        onView(withId(R.id.layout_create_event_fragment)).perform(swipeDown())
+        onView(withId(R.id.submit_create_event)).perform(click())
+
+        // Find modified event and check time to ensure it is all-day
+        onView(withId(R.id.navigation_dashboard)).perform(click())
+        onView(withId(R.id.event_list_recycler_view)).perform(ScrollToBottom())
+        onView(withText(testTitle)).perform(click())
+        onView(withText("All-Day")).check(matches(isDisplayed()))
+        onView(withText("Edit")).perform(click())
+
+        // Delete event
+        onView(withId(R.id.delete_event)).perform(scrollTo(), click())
+        onView(withText("Confirm")).perform(click())
     }
 }
